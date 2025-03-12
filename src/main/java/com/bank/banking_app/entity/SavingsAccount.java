0ff921fac15b532cfcafd95f97f07596bd711826
orugantik63 <oruganti.k63@gmail.com>
2025-03-12 12:20:15 +0530

    adding entities

---
+++ src/main/java/com/bank/banking_app/entity/SavingsAccount.java
@@ -0,0 +1,20 @@
+package com.bank.banking_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavingsAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
